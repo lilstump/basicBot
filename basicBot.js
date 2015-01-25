cbbf4096d89bd2f8e9a0a7814b46df1e7c2cc57e
lilstump <myturtle225@gmail.com>
2015-01-24 16:38:12 -0800

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2819,7 +2819,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         if (typeof basicBot.settings.website === "string")
-                            API.sendChat(subChat(basicBot.chat.website, {link: basicBot.settings.website}));
+                            API.sendChat("BOBERT SAYS FUCK NO");
                     }
                 }
             },

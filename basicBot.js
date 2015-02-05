2d362875a8aea037c13b865e9496765e716c929b
Mightyena002 <tylercourt96@gmail.com>
2015-02-05 22:21:31 +0000

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2818,7 +2818,6 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        if (typeof basicBot.settings.website === "string")
                             API.sendChat(subChat(basicBot.chat.website, {link: basicBot.settings.website}));
                     }
                 }

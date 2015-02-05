ab46e7393127bcee724522d718a2ae6105798e9e
lilstump <myturtle225@gmail.com>
2015-02-05 13:35:44 -0800

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2819,7 +2819,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         if (typeof basicBot.settings.website === "string")
-                            API.sendChat(subChat(basicBot.chat.website, {link: basicBot.settings.website}));
+                        API.sendChat(subChat(basicBot.chat.website, {link: basicBot.settings.website}));
                     }
                 }
             },
@@ -2832,7 +2832,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                                 API.sendChat("domnian.com is the server ip!");
+                         API.sendChat("domnian.com is the server ip!");
                     }
                 }
             },

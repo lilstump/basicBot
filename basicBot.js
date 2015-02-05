a86b850f135a92e15a306d4fa8326b05e0dbf4e5
lilstump <myturtle225@gmail.com>
2015-02-05 13:24:41 -0800

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2825,15 +2825,14 @@
             },
             
            minecraftCommand: {
-                command: 'minecraft','mc',
-                rank: 'residentdj',
+                command: ['minecraft','mc',]
+                rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-
-                                  API.sendChat("domnian.com is the server ip!");
+                                 API.sendChat("domnian.com is the server ip!");
                     }
                 }
             },

fa519a6e6eee7c87b4bc1b6392e645f2d249329a
lilstump <myturtle225@gmail.com>
2015-02-05 13:22:40 -0800

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2812,14 +2812,14 @@
 
             websiteCommand: {
                 command: 'website',
-                rank: 'residentdj',
+                rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-
-                                  API.sendChat(subChat(basicBot.chat.website, {link: basicBot.settings.website}));
+                        if (typeof basicBot.settings.website === "string")
+                            API.sendChat(subChat(basicBot.chat.website, {link: basicBot.settings.website}));
                     }
                 }
             },

4a2640540b329f626dead9548c8719fb48a893d1
lilstump <myturtle225@gmail.com>
2015-11-12 09:57:07 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2830,7 +2830,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat("/me Ask the owner");
+                        API.sendChat("/me Connect using this ip Ultramc.net");
                     }
                 }
             },

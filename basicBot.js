7835f25df87e7a3a6106b8e220492121eda3d4a7
lilstump <noah34916@gmail.com>
2016-11-15 13:11:09 -0500

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2817,19 +2817,6 @@
                 }
             },
             
-            minecraftCommand: {
-                command: ['minecraft','mc'],
-                rank: 'residentdj',
-                type: 'exact',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        API.sendChat("/me Connect using this ip Ultramc.net");
-                    }
-                }
-            },
-        
             danceCommand: {
                 command: 'dance',
                 rank: 'residentdj',
